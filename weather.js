e8016073f659845282da9906b0bd6b0c0d1a7f83
arielstouter-lab <ariel.stouter@gmail.com>
2026-09-16 19:09:37 -0600

    Comment out weather place and icon in weather.js
    Commented out the weather place and icon display in the weather card.

--- weather.js
+++ weather.js
@@ -178,13 +178,14 @@ function renderWeatherCard(raw, place){
         </div>`;
     }).join("")}
     </div>
+  `;
 
-          <div class="wc-place">
+ /*   <div class="wc-place">
         ${place}
         <span class="wc-updated">Updated ${new Date().toLocaleTimeString([], {hour:'numeric', minute:'2-digit'})}</span>
       </div>
-      <div class="wc-icon">${weatherIcon(info.icon)}</div>
-  `;
+      <div class="wc-icon">${weatherIcon(info.icon)}</div> */
+
 
     document.getElementById("wcUnitToggle").addEventListener("click", () => {
         wcUnit = wcUnit === "f" ? "c" : "f";
